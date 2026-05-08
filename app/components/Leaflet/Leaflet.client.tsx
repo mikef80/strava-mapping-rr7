137e45d633d8e7aacf49988ad194c48a40a4a6bb
Mike <mike@mike-francis.org>
2026-05-08 23:02:45 +0100

    updated to center on home and stop map scrolling around the world

--- app/components/Leaflet/Leaflet.client.tsx
+++ app/components/Leaflet/Leaflet.client.tsx
@@ -18,10 +18,16 @@ const Leaflet = ({ activities }: { activities: StravaActivity[] }) => {
   return (
     <div id='map' style={{ height: "100%" }}>
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={[50.255706781193744, -5.289268677584664]}
+        zoom={12}
         scrollWheelZoom={false}
-        style={{ height: "100%", width: "100%" }}>
+        style={{ height: "100%", width: "100%" }}
+        worldCopyJump
+        maxBounds={[
+          [-90, -180],
+          [90, 180],
+        ]}
+        maxBoundsViscosity={0.8}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
