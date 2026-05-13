cb035238b7b5d5f9ae490a1f1b4cf8ad58b73c24
Mike <mike@mike-francis.org>
2026-05-13 21:43:55 +0100

    many feature updates so far! Column soprting and filtering on exercise type...

--- app/components/DataTable/columns.tsx
+++ app/components/DataTable/columns.tsx
@@ -2,11 +2,20 @@ import type { ColumnDef } from "@tanstack/react-table";
 import { format } from "date-fns";
 
 import type { StravaActivity } from "~/types/strava";
+import { Button } from "../ui/button";
 
 export const columns: ColumnDef<StravaActivity>[] = [
   {
     accessorKey: "start_date",
-    header: "Start Date",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+          Start Date
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       const formatted = format(new Date(row.getValue("start_date")), "d MMM yyyy");
       return <div>{formatted}</div>;
@@ -14,7 +23,15 @@ export const columns: ColumnDef<StravaActivity>[] = [
   },
   {
     accessorKey: "type",
-    header: "Exercise Type",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+          Exercise Type
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       return <div>{row.getValue("type")}</div>;
     },
@@ -22,7 +39,15 @@ export const columns: ColumnDef<StravaActivity>[] = [
   {
     id: "distance_miles",
     accessorFn: (row) => row.distance / 1609.34,
-    header: "Distance (mi)",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+          Distance (mi)
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       return <div>{Number(row.getValue("distance_miles")).toFixed(2)}</div>;
     },
@@ -30,7 +55,15 @@ export const columns: ColumnDef<StravaActivity>[] = [
   {
     id: "distance_kilometres",
     accessorFn: (row) => row.distance / 1000,
-    header: "Distance (km)",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+          Distance (km)
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       return <div>{Number(row.getValue("distance_kilometres")).toFixed(2)}</div>;
     },
