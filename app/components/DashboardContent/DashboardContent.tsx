0a71b1b113d61426ee9bb6f37693257565951df7
Mike <mike@mike-francis.org>
2026-05-13 20:34:32 +0100

    Adds DataTable to DashboardContent

--- app/components/DashboardContent/DashboardContent.tsx
+++ app/components/DashboardContent/DashboardContent.tsx
@@ -2,11 +2,14 @@ import { Suspense } from "react";
 import { ClientOnly } from "../ClientOnly/ClientOnly";
 import Leaflet from "../Leaflet/Leaflet.client";
 import type { StravaActivity } from "~/types/strava";
+import { DataTable } from "../DataTable/DataTable";
+import { columns } from "../DataTable/columns";
 
 const DashboardContent = ({ activities }: { activities: StravaActivity[] }) => {
   return (
     <main className='h-full flex flex-col p-4 gap-4'>
       <h1 className='text-2xl'>Strava</h1>
+      <DataTable columns={columns} data={activities} />
       <ClientOnly>
         {() => (
           <Suspense fallback={<div>Loading map...</div>}>
